28a7a9062cf6015ea8dafc1c16c3ed386ead7015
Maxim <fedykmaxim@gmail.com>
2022-10-19 17:39:54 +0300

    commit end19.10.22

---
+++ DesignCodeIOS15/Styles.swift
@@ -0,0 +1,14 @@
+//
+//  Styles.swift
+//  DesignCodeIOS15
+//
+//  Created by Maxim on 19.10.2022.
+//
+
+import SwiftUI
+
+struct Styles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
