030f7e1b33b792977cd234b1d1d23390a814502d
Maxim <fedykmaxim@gmail.com>
2022-10-25 17:56:28 +0300

    commitLessons20

---
+++ DesignCodeIOS15/Tab.swift
@@ -0,0 +1,30 @@
+//
+//  Tab.swift
+//  DesignCodeIOS15
+//
+//  Created by Maxim on 25.10.2022.
+//
+
+import SwiftUI
+
+struct TabItem: Identifiable {
+    var id = UUID()
+    var text: String
+    var icon: String
+    var tab: Tab
+    var color: Color
+    
+}
+var tabItems = [
+    TabItem(text: "Learn Now", icon: "house", tab: .home, color: .teal),
+    TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore, color: .blue),
+    TabItem(text: "Notifications", icon: "bell", tab: .notifications, color: .red),
+    TabItem(text: "Library", icon: "rectangle.stack", tab: .library, color: .pink)
+]
+
+enum Tab: String {
+    case home
+    case explore
+    case notifications
+    case library
+}
